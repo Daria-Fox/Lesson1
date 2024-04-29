91f9ad264e07f603c5f7339bee41149ff75f3557
Daria-Fox <167777808+Daria-Fox@users.noreply.github.com>
2024-04-29 08:24:48 +0300

    Add files via upload

--- Lesson1.py
+++ Lesson1.py
@@ -1,7 +1,7 @@
 text = 'это моя строка'
 print(text[0])
 print(text[-1])
-print(text[2:4])
+print(text[3:6])
 print(text[::-1])
 print(len(text))
-print("это новая строка" + (text))
+print("это новая строка " + (text))
